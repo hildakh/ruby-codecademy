f46da18a927495203f54b238c19761f1e6ef44a4
Hilda <hilda.kh.amiri@gmail.com>
2019-12-23 19:14:34 -0800

    C'est fini!

--- THITH MEANTH WAR!.rb
+++ THITH MEANTH WAR!.rb
@@ -1,4 +1,14 @@
 print "Type thomething interethting pleathe!"
 user_input = gets.chomp
+if user_input.length == 0
+  puts "Don't be shy! Say somthing!"
+  user_input = gets.chomp
+end
 user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else puts "Didn't find anything interesting!"
+end
+
 puts user_input
