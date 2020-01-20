c4f79929fcffdf482757ced55882539d68294d25
Hilda <hilda.kh.amiri@gmail.com>
2020-01-20 13:57:36 -0800

    movie update, delete and display cases completed

--- movies.rb
+++ movies.rb
@@ -14,14 +14,30 @@ when "add"
   title = gets.chomp.to_sym
   puts "What rating does the movie have? "
   rating = gets.chomp.to_i
+  if (movies[title])
+    puts "This movie is already in the collection!"
+  else
   movies[title] = rating
   puts "A new movie was added to the collection!"
+  end
 when "update"
-  puts "Updated!"
+  puts "Which movie would you like to update?"
+  title = gets.chomp.to_sym
+  if (movies[title] == nil)
+    puts "This movie is not in the collection"
+  else
+    puts "What's the new rating?"
+    rating = gets.chomp.to_i
+    movies[title] = rating
+  end
 when "display"
-  puts "Movies!"
+  movies.each { |k, v| puts "#{k}: #{v}"}
 when "delete"
-  puts "Deleted!"
+  puts "Which movie would you like to remove from the collection?"
+  title = gets.chomp.to_sym
+  movies.delete(title)
+  # to test the removal of the key/value pair
+  # movies.each {|k, v| puts "#{k}: #{v}"}
 else
   puts "Error!"
 end
