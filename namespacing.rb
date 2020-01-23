447d315c21633060917b62e8f0377b7ab084ca38
Hilda <hilda.kh.amiri@gmail.com>
2020-01-23 15:06:14 -0800

    ex on namespacing and using ::

---
+++ namespacing.rb
@@ -0,0 +1,3 @@
+#keeping modules and constants in one place in order not to confuse them with similar modules and constants is called namespacing :: is called the "scope resolution operator"
+
+puts Math::PI
